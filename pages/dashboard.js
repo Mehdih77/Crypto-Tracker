129c7e15b9d69725fbab4c12b84efd33c02b4314
Mehdih77 <mehdiwork77@gmail.com>
2021-07-26 13:38:20 +0430

    Start Style for Dashboard Page

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,27 @@
+import styles from '../styles/Dashboard.module.css'
+
 function Dashboard() {
     return (
-        <div>
-            
+        <div className={styles.dashboard}>
+                <div className={styles.dashboard_card}>
+                    <div className={styles.dashboard_card_top}>
+                        <img src="./img/user.png" alt="user" />
+                    </div>
+                    <div className={styles.dashboard_card_middle}>
+                            <p className={styles.user_name}> Mehdi <span className={styles.user_age}>24</span></p>
+                            <p className={styles.user_country}> Iran </p>
+                    </div>
+                    <div className={styles.dashboard_card_bottom}>
+                        <div className={styles.dashboard_card_bottom_body} >
+                            <p>$66000</p>
+                            <p>Income</p>
+                        </div>
+                        <div className={styles.dashboard_card_bottom_body}>
+                            <p>$8000</p>
+                            <p>Expense</p>
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 }
